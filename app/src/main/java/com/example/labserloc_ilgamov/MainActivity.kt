2e30f50d4cf0cb4f9cacff4a8617517bd756a0da
ILGAMOV <r.ilgamov@yandex.ru>
2021-05-13 23:45:56 +0500

    Генерация рандомных чисел сервис

--- app/src/main/java/com/example/labserloc_ilgamov/MainActivity.kt
+++ app/src/main/java/com/example/labserloc_ilgamov/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.labserloc_ilgamov
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.ActivityManager
+import android.content.Context
+import android.widget.Toast
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-} 1
+class MainActivity : AppCompatActivity()
---
+++ app/src/main/java/com/example/labserloc_ilgamov/RandomNumberService.kt
@@ -0,0 +1,40 @@
+package com.example.labserloc_ilgamov
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+class RandomNumberService : Service() {
+
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+
+    override fun onBind(intent: Intent): IBinder? {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+
+        toast("Service started.")
+
+
+        mHandler = Handler()
+        mRunnable = Runnable { showRandomNumber() }
+        mHandler.postDelayed(mRunnable, 5000)
+
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toast("Service destroyed.")
+        mHandler.removeCallbacks(mRunnable)
+    }
+
+
+    private fun showRandomNumber() {
+        val rand = Random()
+        val number = rand.nextInt(100)
+        toast("Random Number : $number")
+        mHandler.postDelayed(mRunnable, 5000)
+    }
+}
