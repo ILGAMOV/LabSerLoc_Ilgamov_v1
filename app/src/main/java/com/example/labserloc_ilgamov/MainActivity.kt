4b2d48ef08c715fcb40802066ff504efd10ef4ab
ILGAMOV <r.ilgamov@yandex.ru>
2021-05-14 17:00:39 +0500

    Локация импортирована в проект

--- app/src/main/java/com/example/labserloc_ilgamov/MainActivity.kt
+++ app/src/main/java/com/example/labserloc_ilgamov/MainActivity.kt
@@ -6,18 +6,24 @@ import android.app.ActivityManager
 import android.content.Context
 import android.widget.Toast
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
-    //Локация
+//Локация
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//Локация
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
 
         val serviceClass = RandomNumberService::class.java
         val intent = Intent(applicationContext, serviceClass)
@@ -52,6 +58,24 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun fetchLocation()
+    {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it !=null){
+                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }
+    }
+    }
+
 
     private fun isServiceRunning(serviceClass: Class<*>): Boolean {
         val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
